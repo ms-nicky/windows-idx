e89eae99ec72b96ca10109e8ed4d787a360d87d5
ms-nicky <nickystore603@gmail.com>
2026-02-01 07:45:26 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,27 +1,21 @@
 { pkgs, ... }:
 
 {
-  # Danh sách package cài sẵn
   packages = with pkgs; [
-    # QEMU đầy đủ (có qemu-system-x86_64)
     qemu_full
-
-
+    curl
     wget
-
-    # Tunnel
-    ngrok
-    
+    tailscale
   ];
+
   idx.workspace.onStart = {
-    run-ngrok = ''
-      cd /usr
+    prepare = ''
       cp /home/user/windows-idx/nicky.sh /nicky.sh
-      chmod +x /run.sh
-      bash /nicky.sh
+      chmod +x /nicky.sh
+      bash nicky.sh
     '';
   };
-  # Biến môi trường (an toàn với IDX)
+
   env = {
     QEMU_AUDIO_DRV = "none";
   };
