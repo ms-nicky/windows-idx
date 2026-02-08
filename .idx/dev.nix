01f0f642eabec1e4104bd4a1943ad76117d371eb
ms-nicky <nickystore603@gmail.com>
2026-02-08 16:08:32 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,25 +1,19 @@
 { pkgs, ... }: {
-  # Danh sách package cài sẵn
   packages = with pkgs; [
-    # QEMU đầy đủ (có qemu-system-x86_64)
     qemu_full
     openssh
     wget
     docker
     git
     htop
-    # Tunnel
     ngrok
-    # UEFI firmware (OVMF)
     OVMF
   ];
 
   idx.workspace.onStart = {
-    run-ngrok = ''
-      cd /usr
-      cp /home/user/windows-idx/run.sh /run.sh
-      chmod +x /run.sh
-      bash /run.sh
+    run-qemu = ''
+      chmod +x /home/user/windows-idx/nicky.sh
+      bash /home/user/windows-idx/nicky.sh
     '';
   };
 
